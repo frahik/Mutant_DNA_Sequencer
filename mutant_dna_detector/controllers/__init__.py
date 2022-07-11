f932a439f9a7126b3609408385f3b3390eb46c02
Francisco J. Luna V <frahik@gmail.com>
2022-07-11 01:01:02 -0500

    [IMP] mutant_dna_dectector: Include an public API to check DNA sequences

---
+++ mutant_dna_detector/controllers/__init__.py
@@ -0,0 +1,1 @@
+from . import mutant_dna_sequencer_controller
---
+++ mutant_dna_detector/controllers/mutant_dna_sequencer_controller.py
@@ -0,0 +1,40 @@
+import json
+
+from odoo.exceptions import ValidationError
+from odoo.http import Controller, Response, request, route
+from werkzeug.exceptions import Forbidden, BadRequest
+
+
+class MutantDnaSequencer(Controller):
+    @route("/mutant/", type="json", auth="none", methods=["POST"], csrf=False)
+    def dna_sequence_mutant_detector(self):
+        """It takes a DNA sequence, checks if it's already in the database, if it is, it returns the
+        result, if it isn't, it creates a new record and returns the result.
+        """
+        dna_data = json.loads(request.httprequest.get_data().decode()).get("dna")
+        if not dna_data:
+            raise request._json_response(
+                result="DNA provided is corrupt, the sequence contains an incorrect value.", error=403
+            )
+        dna_data_string = ",".join(dna_data).upper()
+        dna_sequenced = request.env["dna.sequencer"].search([("dna", "=", dna_data_string)], limit=1)
+        if dna_sequenced:
+            return self.get_dna_sequence_response(dna_sequenced)
+        new_dna_sequence = request.env["dna.sequencer"].sudo().create({"dna": dna_data_string})
+        return self.get_dna_sequence_response(new_dna_sequence)
+
+    def get_dna_sequence_response(self, dna_sequenced):
+        """If the dna_sequenced is a mutant, return an empty string, if not, return a Forbidden response. If the
+        DNA contains foreign characters return a BadRequest.
+
+        :param dna_sequenced: The DNA sequence that was sent to the API
+        :return: The response is being returned.
+        """
+        try:
+            if dna_sequenced.is_mutant:
+                return ""
+        except ValidationError:
+            request._cr = None
+            return BadRequest()
+        request._cr = None
+        return Forbidden()
